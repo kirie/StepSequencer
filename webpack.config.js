b9e11072aa73535d9a2731b4e3fbb0d51e994cbd
Eirik <eiriklin@yahoo.com>
2017-01-17 15:43:15 -0800

    Add - Webpack and more npm script setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      }
+    ]
+  }
+};
