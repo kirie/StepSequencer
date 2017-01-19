1e76b5f89f385c8fd13c2f5e8e683fe4db832464
Eirik <eiriklin@yahoo.com>
2017-01-19 11:51:46 -0800

    Add - Screw & Toggle Components

---
+++ src/components/screws.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <div className="screwplate"><div className="screw" /><div className="screw" /></div>;
---
+++ src/components/toggle.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Toggle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggled: this.props.value
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange() {
+    this.props.abfunc();
+    this.setState({ toggled: !this.state.toggled });
+  }
+
+  render() {
+    let toggleClasses = 'togglebox';
+    if (this.state.toggled) {
+      toggleClasses += ' toggled';
+    }
+
+    return (
+      <div
+        className={toggleClasses}
+        onClick={this.handleChange}
+      >
+        <div className="lettera">A</div>
+        <div className="toggle-slider">
+          <div className="toggle-slider-button" />
+        </div>
+        <div className="letterb">B</div>
+
+      </div>
+    );
+  }
+}
+
+export default Toggle;
