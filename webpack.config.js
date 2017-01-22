13daeb965163eb002854c88dd8a8c06852736abf
Eirik <eiriklin@yahoo.com>
2017-01-21 18:33:46 -0800

    Dist3

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].[chunkhash].js',
-    publicPath: './StepSequencer/dist/'
+    publicPath: './dist/'
   },
   module: {
     rules: [
